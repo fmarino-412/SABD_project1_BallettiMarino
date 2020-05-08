8d22baeb098c36d7bd9b945a07a1cc5bad4ecfd4
Francesco Marino <francesco.marino.412@alumni.uniroma2.eu>
2020-05-08 14:26:21 +0200

    Docker environment

--- docker-env/start-dockers.sh
+++ docker-env/start-dockers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker network create --driver bridge apache_network
-docker run -p 9880:9880 -d --network=apache_network -v nifi_conf:/opt/nifi/nifi-current/conf -v nifi_state:/data/nifi/nifi-current/state -e NIFI_WEB_HTTP_PORT='9880' --name=nifi apache/nifi
+docker run -p 9880:9880 -d --network=apache_network -v nifi_conf:/opt/nifi/nifi-current/conf -v nifi_state:/opt/nifi/nifi-current/state -v nifi_hdfs:/opt/nifi/nifi-current/hdfs -e NIFI_WEB_HTTP_PORT='9880' --name=nifi apache/nifi
+docker cp nifi-hdfs nifi:/opt/nifi/nifi-current/hdfs
 docker run -t -i -p 9864:9864 -d --network=apache_network --name=slave1 effeerre/hadoop
 docker run -t -i -p 9863:9864 -d --network=apache_network --name=slave2 effeerre/hadoop
 docker run -t -i -p 9862:9864 -d --network=apache_network --name=slave3 effeerre/hadoop
