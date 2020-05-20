916b7d834d791d68c4a426ab5279973a2666c0fd
marcobaleno96 <marco.balletti1996@gmail.com>
2020-05-20 16:07:43 +0200

    Query2SparkSQL updated, work but still miss stddev

--- src/main/java/query2/Query2SparkSQL.java
+++ src/main/java/query2/Query2SparkSQL.java
@@ -4,13 +4,20 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 import utility.ContinentDecoder;
 import utility.IOUtility;
 import utility.QueryUtility;
-
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Query2SparkSQL {
 
@@ -47,5 +54,44 @@ public class Query2SparkSQL {
 
                 }
         );
+
+        SparkSession session = SparkSession
+                .builder()
+                .appName("Query 2 SparkSQL")
+                .master("local")
+                .getOrCreate();
+
+        Dataset<Row> dataFrame = createSchema(session, dailyData);
+
+        dataFrame.groupBy("continent", "date")
+                .avg("positive").as("average").show();
+        dataFrame.groupBy("continent", "date")
+                .min("positive").as("minimum").show();
+        dataFrame.groupBy("continent", "date")
+                .max("positive").as("maximum").show();
+
+        IOUtility.printTime(System.currentTimeMillis() - startTime);
+
+        session.close();
+        sparkContext.close();
+    }
+
+    private static Dataset<Row> createSchema(SparkSession session, JavaPairRDD<String, Tuple2<String, Double>> data) {
+
+        // Generating schema
+        List<StructField> fields = new ArrayList<>();
+        fields.add(DataTypes.createStructField("id", DataTypes.LongType, false));
+        fields.add(DataTypes.createStructField("continent", DataTypes.StringType, false));
+        fields.add(DataTypes.createStructField("date", DataTypes.StringType, false));
+        fields.add(DataTypes.createStructField("positive", DataTypes.DoubleType, false));
+
+        StructType schema = DataTypes.createStructType(fields);
+
+        // Convert RDD records to Rows
+        JavaRDD<Row> rowRDD = data.zipWithIndex().map(element -> RowFactory.create(element._2(), element._1()._1(),
+                element._1()._2()._1(), element._1()._2()._2()));
+
+        // Apply schema to RDD and return
+        return session.createDataFrame(rowRDD, schema);
     }
 }
