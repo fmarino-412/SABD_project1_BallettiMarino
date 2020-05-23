e7aa784a96814baa4ecace782bd1ba3142f66690
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-05-23 16:09:57 +0200

    Docker scripts edit

--- docker-env/stop-dockers.sh
+++ docker-env/stop-dockers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker kill master nifi slave1 slave2 slave3 hbase grafana rabbitmq influxdb
-docker rm nifi master slave1 slave2 slave3 hbase grafana rabbitmq influxdb
+docker kill master nifi slave1 slave2 slave3 hbase grafana influxdb
+docker rm nifi master slave1 slave2 slave3 hbase grafana influxdb
 docker network rm apache_network
